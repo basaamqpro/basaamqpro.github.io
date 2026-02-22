ae5d95e8ee0ff8169705cab950b98dbb44cd382c
basaamqpro <basaamqpro@gmail.com>
2026-02-22 17:23:56 +0000

    convert react v10 part 1

--- basaamqpro_github_backup/React/my-app/next.config.ts
+++ basaamqpro_github_backup/React/my-app/next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  output: "export",   // 👈 this enables static export
+const nextConfig = {
+  output: "export",
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
