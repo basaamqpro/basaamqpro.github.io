0b00f4dec4ec6e456d1d02fcdb76b195d211953b
basaamqpro <basaamqpro@gmail.com>
2026-03-12 23:34:46 +0000

    updated portfolio 8

--- basaamqpro_github_backup/React/my-app/app/row_one.tsx
+++ basaamqpro_github_backup/React/my-app/app/row_one.tsx
@@ -1,48 +1,74 @@
-"use client"; // <-- Add this line at the very top
+"use client";
 
-import React from "react";  
-import './row_one.css';
+import React from "react";
+import "./row_one.css";
 
 export default function Row1() {
   return (
     <div className="rowOne">
-        <LeftColumn />
-        <RightColumn />
+      <LeftColumn />
+      <RightColumn />
     </div>
   );
 }
 
-
 function LeftColumn() {
-    return (
-        <div className="left-column">
-            <div className="container">
-            <div className="row0 row">
-                <h1 className="hello">Hello</h1>
-                <div className="dot"></div>
-            </div>
-            <div className="row1 row">
-                <div className="line"></div>
-                <h1 className="name">I'm Basaam Q</h1>
-            </div>
-            <div className="row2">
-                <h1 className="Entry">Entry Web Developer</h1>
-            </div>
-            <div className="row3 row">
-                <a onClick={() => window.scrollTo({ top: document.body.scrollHeight, behavior: 'smooth' })} className="Hire contactbutton">
-                <button onClick={() => window.scrollTo({ top: document.body.scrollHeight, behavior: 'smooth' })} className="Hire contactbutton">Hire Me!</button>
-                </a>
-                <a href="basaamq_cv.pdf"><button className="Resume contactbutton">My Resume</button></a>
-            </div>
-            </div>
+  const scrollToBottom = () => {
+    window.scrollTo({
+      top: document.body.scrollHeight,
+      behavior: "smooth"
+    });
+  };
+
+  return (
+    <div className="left-column">
+      <div className="container">
+
+        <div className="row0 row">
+          <h1 className="hello">Hello</h1>
+          <div className="dot"></div>
         </div>
-    );
+
+        <div className="row1 row">
+          <div className="line"></div>
+          <h1 className="name">I'm Basaam Q</h1>
+        </div>
+
+        <div className="row2">
+          <h1 className="Entry">Entry Web Developer</h1>
+        </div>
+
+        <div className="row3 row">
+          <button onClick={scrollToBottom} className="Hire contactbutton">
+            Hire Me!
+          </button>
+
+          <a href="/basaamq_cv.pdf">
+            <button className="Resume contactbutton">My Resume</button>
+          </a>
+        </div>
+
+      </div>
+    </div>
+  );
 }
 
 function RightColumn() {
-    return (
-        <div className="right-column">
-           <img src="/basaam.png" alt="Profile Picture" className="profile_image" />
-        </div>
-    );
+  return (
+    <div className="right-column">
+
+     
+    <div id="youtube_video_container">
+      <iframe
+        className="youtube_video"
+        src="https://www.youtube.com/embed/IC0wcKV5NXk"
+        title="YouTube video player"
+        frameBorder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+        referrerPolicy="strict-origin-when-cross-origin"
+        allowFullScreen
+      ></iframe>
+    </div>
+    </div>
+  );
 }
